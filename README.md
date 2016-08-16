cc2c0efec987df7ecad31b0f42cdeb86c584c123
Joseph David Borġ <joe@josephb.org>
2016-08-16 18:23:31 +0100

    Adding README

---
+++ README.md
@@ -0,0 +1,10 @@
+JSONProxy
+=========
+
+JSONProxy allows you to proxy APIs that only support JSON to support JSONP.
+
+
+TODO
+----
+- Add support for all request methods
+- Add CLI arguments
--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
+    "fmt"
+    "io/ioutil"
+    "net/http"
 )
 
 const baseURL string = "http://127.0.0.1:8000/api"
 
 func requestGet(p string) []byte {
-	resp, _ := http.Get(baseURL + p)
-	defer resp.Body.Close()
+    resp, _ := http.Get(baseURL + p)
+    defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	return body
+    body, _ := ioutil.ReadAll(resp.Body)
+    return body
 }
 
 func requestPost(p string, f map[string][]string) []byte {
-	resp, _ := http.PostForm(baseURL+p, f)
-	defer resp.Body.Close()
+    resp, _ := http.PostForm(baseURL+p, f)
+    defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	return body
+    body, _ := ioutil.ReadAll(resp.Body)
+    return body
 }
 
 func receiveRequest(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		body := requestGet(r.URL.Path)
-		if len(r.FormValue("callback")) > 0 {
-			fmt.Fprintf(w, "%s(%s)", r.FormValue("callback"), body)
-		} else {
-			w.Write(body)
-		}
+    switch r.Method {
+    case "GET":
+        body := requestGet(r.URL.Path)
+        if len(r.FormValue("callback")) > 0 {
+            fmt.Fprintf(w, "%s(%s)", r.FormValue("callback"), body)
+        } else {
+            w.Write(body)
+        }
 
-	case "POST":
-		body := requestPost(r.URL.Path, r.Form)
-		if len(r.FormValue("callback")) > 0 {
-			fmt.Fprintf(w, "%s(%s)", r.FormValue("callback"), body)
-		} else {
-			w.Write(body)
-		}
-	}
+    case "POST":
+        body := requestPost(r.URL.Path, r.Form)
+        if len(r.FormValue("callback")) > 0 {
+            fmt.Fprintf(w, "%s(%s)", r.FormValue("callback"), body)
+        } else {
+            w.Write(body)
+        }
+    }
 }
 
 func main() {
-	http.HandleFunc("/", receiveRequest)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", receiveRequest)
+    http.ListenAndServe(":8080", nil)
 }
